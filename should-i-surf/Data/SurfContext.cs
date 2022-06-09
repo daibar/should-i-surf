299d47ed63c74d30d56712cd54f201780870f86b
daibar <dannyaibar@gmail.com>
2022-06-08 23:13:01 -0700

    Added Dbcontext and EF dependencies

---
+++ should-i-surf/Data/SurfContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using should_i_surf.Models;
+
+namespace should_i_surf.Data
+{
+    public class SurfContext : DbContext
+    {
+        public SurfContext(DbContextOptions<SurfContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<Forecast> Forecasts { get; set; }
+        
+    }
+}
--- should-i-surf/Startup.cs
+++ should-i-surf/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using should_i_surf.Data;
+using should_i_surf.Models;
 
 namespace should_i_surf
 {
@@ -26,6 +28,9 @@ namespace should_i_surf
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SurfContext>(opt => opt.UseNpgsql
+                (Configuration.GetConnectionString("SurfConnection")));
+            
             services.AddControllers();
 
             services.AddScoped<IForecastRepo, MockSurfRepo>();
