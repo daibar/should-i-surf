46c7a26e24d7583a48879efe2c73eaf961d56334
daibar <dannyaibar@gmail.com>
2022-06-12 22:43:45 -0700

    Initial migration

---
+++ should-i-surf/Migrations/20220611050622_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace should_i_surf.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forecasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SwellRating = table.Column<int>(nullable: false),
+                    WeatherRating = table.Column<int>(nullable: false),
+                    Outlook = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forecasts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Forecasts");
+        }
+    }
+}
--- should-i-surf/Models/Forecast.cs
+++ should-i-surf/Models/Forecast.cs
@@ -1,10 +1,14 @@
-﻿namespace should_i_surf.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace should_i_surf.Models
 {
     public class Forecast
     {
         public int Id { get; set; }
         public int SwellRating { get; set; }
         public int WeatherRating { get; set; }
+        
+        [Required]
         public string Outlook { get; set; }
     }
 }
