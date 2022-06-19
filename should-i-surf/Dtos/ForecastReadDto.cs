cbc2e5eb6f76a4805f028da33a080f47048cce32
Danny Aibar <dannyaibar@gmail.com>
2022-06-18 21:58:23 -0700

    Added Automapper and initial Forecast Dto

---
+++ should-i-surf/Dtos/ForecastReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace should_i_surf.Dtos
+{
+    public class ForecastReadDto
+    {
+        public int Id { get; set; }
+        public int SwellRating { get; set; }
+        public int WeatherRating { get; set; }
+        public string Outlook { get; set; }
+    }
+}
